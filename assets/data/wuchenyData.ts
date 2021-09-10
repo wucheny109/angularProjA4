2bb2844c7792dacdc3c1ce2db448ffb34169beb7
wucheny109 <59895756+wucheny109@users.noreply.github.com>
2021-09-10 15:22:50 -0400

    Add files via upload

---
+++ assets/data/wuchenyData.ts
@@ -0,0 +1,9 @@
+import { Wucheny } from "src/app/type";
+
+export const MYINFO : Wucheny = {
+  sid: 991566725,
+  sname: 'Chen-Yu Wu',
+  slogin: 'wucheny',
+  scampus: 'Davis',
+  sassignTitle: 'wuchenyA4'
+};
